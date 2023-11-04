7df2a8c38d84b14c29b5a485971c79519a6114fc
YUGA <113231660+danfunc@users.noreply.github.com>
2023-11-04 09:23:26 +0000

    修正

--- kernel/src/processors/rp2040/entry_func.cpp
+++ kernel/src/processors/rp2040/entry_func.cpp
@@ -7,7 +7,7 @@
 
 void shizuku::types::processors::rp2040::cpu_driver::entry_func(
     void (*entry)(), context &context) {
-  void *sp = (void *)((int)malloc(4 * 1024) + 4 * 1024);
+  void *sp = (void *)((int)malloc(1 * 1024) + 1 * 1024);
   sp = (void *)((int)sp & ~0xfL);
   if (save_context(context) == 0) {
     context.sp = sp;
