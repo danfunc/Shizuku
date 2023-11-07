888627c6ec606864ccf11b1ae897814866529a2a
YUGA <113231660+danfunc@users.noreply.github.com>
2023-11-07 10:13:01 +0000

    ツールチェインロード機能の改善

--- load_toolchain.cmake
+++ load_toolchain.cmake
@@ -1,6 +1,11 @@
 if(DEFINED ENV{SHIZUKU_TOOLCHAIN})
-    include(${CMAKE_CURRENT_SOURCE_DIR}/toolchains/$ENV{SHIZUKU_TOOLCHAIN}.cmake)
-    message(loadchain)
+  if(EXISTS
+     ${CMAKE_CURRENT_SOURCE_DIR}/toolchains/$ENV{SHIZUKU_TOOLCHAIN}.cmake)
+    include(
+      ${CMAKE_CURRENT_SOURCE_DIR}/toolchains/$ENV{SHIZUKU_TOOLCHAIN}.cmake)
+  endif()
 elseif(DEFINED SHIZUKU_TOOLCHAIN)
+  if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/toolchains/${SHIZUKU_TOOLCHAIN}.cmake)
     include(${CMAKE_CURRENT_SOURCE_DIR}/toolchains/${SHIZUKU_TOOLCHAIN}.cmake)
+  endif()
 endif()
