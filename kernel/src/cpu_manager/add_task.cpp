0f6003009022484b37d0de11b3495ef94afd7889
YUGA <113231660+danfunc@users.noreply.github.com>
2023-11-30 21:59:22 +0900

    マルチスレッドで遊んでみた

--- kernel/src/cpu_manager/add_task.cpp
+++ kernel/src/cpu_manager/add_task.cpp
@@ -3,6 +3,6 @@
 void shizuku::types::cpu_manager::add_task(
     shizuku::platform::std::shared_ptr<shizuku::types::thread> thread,
     size_t processing_time, int priority) {
-  task_queue.push(shizuku::types::task{
-      .thread = thread, .remain_time = processing_time, .priority = priority});
+  task_queue.push(
+      {.thread = thread, .remain_time = processing_time, .priority = priority});
 }
--- kernel/test/multithread_test.cpp
+++ kernel/test/multithread_test.cpp
@@ -10,14 +10,23 @@ int sub_func(int argc, char **argv);
 void multi_thread_entry() {
   shizuku::platform::std::shared_ptr<shizuku::types::thread>
       main_thread = manager.get_current_thread(),
-      sub_thread = std::make_shared<shizuku::types::thread>();
-  shizuku::cpu_driver::entry_func(sub_func, sub_thread->context.get(), 3, 0);
-  manager.add_task(sub_thread, 1, 1);
+      sub_thread = std::make_shared<shizuku::types::thread>(),
+      sub2_thread = std::make_shared<shizuku::types::thread>(),
+      sub3_thread = std::make_shared<shizuku::types::thread>(),
+      sub4_thread = std::make_shared<shizuku::types::thread>();
+  shizuku::cpu_driver::entry_func(sub_func, sub_thread->context.get(), 1, 0);
+  shizuku::cpu_driver::entry_func(sub_func, sub2_thread->context.get(), 2, 0);
+  shizuku::cpu_driver::entry_func(sub_func, sub3_thread->context.get(), 3, 0);
+  shizuku::cpu_driver::entry_func(sub_func, sub4_thread->context.get(), 4, 0);
+  manager.add_task(sub_thread, 1, 2);
+  manager.add_task(sub2_thread, 1, 2);
+  manager.add_task(sub3_thread, 1, 2);
+  manager.add_task(sub4_thread, 1, 1);
   manager.set_current_remain(1);
   while (true) {
     printf("main_thread\n");
     sleep_ms(500);
-    manager.add_task(main_thread, 1, 1);
+    manager.add_task(main_thread, 1, 0);
     manager.context_switch();
   }
   return;
