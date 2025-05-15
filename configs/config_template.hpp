890d2ff61306c5a4b586717e5ee375c55a1c577a
YUGA <113231660+danfunc@users.noreply.github.com>
2025-05-15 19:28:04 +0900

    rewriting

--- configs/config_template.hpp
+++ configs/config_template.hpp
@@ -1,10 +1,11 @@
 #ifndef SHIZUKU_CONFIG_TEMPLATE_HPP
 #define SHIZUKU_CONFIG_TEMPLATE_HPP
 
+${INCLUDE_HEADERS_INSTRUCTION}
+
 #include "shizuku/templates/kernel.hpp"
 #include "shizuku/templates/cpu_manager.hpp"
 #include "shizuku/dummy.hpp"
-#include "shizuku/cpu_drivers/${SHIZUKU_CPU_MANAGER}.hpp"
 
 namespace shizuku{
     using kernel = templates::kernel<shizuku::templates::cpu_manager<shizuku::cpu_drivers::${SHIZUKU_CPU_MANAGER},${SHIZUKU_CPU_COUNT}>,shizuku::dummy::object>;
--- modules/pico_sdk_support/main.cpp
+++ modules/pico_sdk_support/main.cpp
@@ -1,5 +1,5 @@
 #include "shizuku/kernel.hpp"
 #include "shizuku/cpu_drivers/rp2040.hpp"
 int main(){
-    shizuku::cpu_drivers::rp2040::get_core_num();
+    
 }
